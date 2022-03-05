5e213bfd4ea1a9dbf28cb42c49bb51ccf56b8e65
andy <andy.development@protonmail.com>
2022-03-05 13:29:19 -0500

    chore: Initial commit

---
+++ README.rst
@@ -0,0 +1,15 @@
+Rain Wrapper
+============
+An API wrapper for `revolt`
+
+
+Goals
+=====
+- Strongly typed `revolt` API wrapper.
+- Good semantics & conventions.
+- Explicit.
+
+
+Notable contributors
+====================
+- [an-dyy](https://github.com/an-dyy) Creater & Maintainer.
---
+++ rain/__init__.py
@@ -0,0 +1,15 @@
+__author__ = "Andy"
+__license__ = "MIT"
+__version__ = '0.1.0-alpha'
+
+from typing import NamedTuple, Literal
+
+class Version(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+
+    id: Literal["alpha", "beta", "final"]
+
+
+version = Version(0, 1, 0, "alpha")
